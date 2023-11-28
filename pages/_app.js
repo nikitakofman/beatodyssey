f45b58590c71a8cc65888812634c9729e10be377
Nikita <kofman.niki@gmail.com>
2023-11-28 16:51:32 +0100

    meta image added

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,36 @@ function App({ Component, pageProps }) {
           name="description"
           content="Beat Odyssey is a virtual drum machine where you can have fun playing different electronic music genres."
         ></meta>
+
+        {/* Open Graph / Facebook Meta Tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://beatodyssey.com/" />
+        <meta property="og:title" content="Beat Odyssey" />
+        <meta
+          property="og:description"
+          content="Beat Odyssey is a virtual drum machine where you can have fun playing different electronic music genres."
+        />
+        <meta
+          property="og:image"
+          content="https://www.yourwebsiteurl.com/path/to/your/image.jpg"
+        />
+
+        {/* You might also want to add Twitter specific tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="beatodyssey.com" />
+        <meta
+          property="twitter:url"
+          content="https://www.yourwebsiteurl.com/"
+        />
+        <meta name="twitter:title" content="Beat Odyssey" />
+        <meta
+          name="twitter:description"
+          content="Beat Odyssey is a virtual drum machine where you can have fun playing different electronic music genres."
+        />
+        <meta
+          name="twitter:image"
+          content="https://www.yourwebsiteurl.com/path/to/your/twitter-image.jpg"
+        />
       </Head>
       <Component {...pageProps} />
     </>
